ed3a305d317760c152822bd9b163dd4e2f3db1ee
Chaiyaone <chaiyanan8308@gmail.com>
2025-03-06 09:59:52 +0700

    #

---
+++ src/assets/ShowName.jsx
@@ -0,0 +1,13 @@
+import Proptypes from "prop-types";
+
+export default function ShowName({name,show}){
+    console.log("name: ", name)
+    console.log("show : ", show)
+
+    return <div>{show && <h2>Your name is: {name}</h2>}</div>
+}
+
+ShowName.Proptypes = {
+    name: Proptypes.string.isRequired,
+    show: Proptypes.bool.isRequired
+}
